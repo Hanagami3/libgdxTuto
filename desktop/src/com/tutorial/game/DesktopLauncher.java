fa83d330b995c613016eee890a59cf66359968ea
Hanagami3 <hanaga3mi@gmail.com>
2024-06-05 13:10:39 +0200

    libgdx basis

---
+++ desktop/src/com/tutorial/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.tutorial.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.tutorial.game.TutorialSpaceGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Tutorial Space Game");
+		new Lwjgl3Application(new TutorialSpaceGame(), config);
+	}
+}
